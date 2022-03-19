6f71cefc0c43f0d7935b8f4d4c9932f8f341f98c
kusambhav <kusambhav@expediagroup.com>
2022-03-19 11:37:45 +0530

    first commit

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../widgets/transaction_item.dart';
+import '../models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No Transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ))
+              ],
+            );
+          })
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return TransactionItem(transaction: transactions[index], deleteTx: deleteTx);
+            },
+            itemCount: transactions.length,
+            // children: transactions.map((tx) {}).toList(),
+          );
+  }
+}
+
+
